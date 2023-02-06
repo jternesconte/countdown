244af04ea043e277a51843b344db12c12c16b311
João Pedro <joaopedrotc04@gmail.com>
2023-02-06 19:21:26 -0300

    seccond commit

--- countdown.py
+++ countdown.py
@@ -1,12 +1,12 @@
 import time
 
-my_time = int(input("Enter the time in seconds: "))
+tempo = int(input("Enter the time in seconds: "))
 
-for x in range(my_time, 0, -1):
-    sec = x % 60
+for x in range(tempo, 0, -1):
+    seg = x % 60
     min = int(x / 60) % 60
-    hour = int(x/3600)
-    print(f"{hour:02}:{min:02}:{sec:02}")
+    hora = int(x/3600)
+    print(f"{hora:02}:{min:02}:{seg:02}")
     time.sleep(1)
 
 print("TIME'S UP!")
